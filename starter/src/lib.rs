488ae5cee2961c243b5eea316c11730e3f447132
john <casle_death@163.com>
2024-11-11 00:26:23 +0800

    移除无用的注释

--- starter/src/lib.rs
+++ starter/src/lib.rs
@@ -53,31 +53,7 @@ pub fn start(
     let t = std::thread::spawn(
         ||{
             RT.block_on(async {
-                // if std::env::var_os("RUST_LOG").is_none() {
-                //     std::env::set_var("RUST_LOG", &CFG.log.log_level);
-                // }
                 my_env::setup();
-
-                // // 系统变量设置
-                // let log_env = my_env::get_log_level();
-                //
-                // //  日志设置
-                // let format = my_env::get_log_format();
-                //
-                // // 文件输出
-                // let file_appender = tracing_appender::rolling::hourly(&CFG.log.dir, &CFG.log.file);
-                // let (non_blocking, _guard) = tracing_appender::non_blocking(file_appender);
-                //
-                // // 标准控制台输出
-                // let (std_non_blocking, _guard) = tracing_appender::non_blocking(std::io::stdout());
-                // let logger = Registry::default()
-                //     .with(EnvFilter::from_default_env().add_directive(log_env.into()))
-                //     .with(fmt::Layer::default().with_writer(std_non_blocking).event_format(format.clone()).pretty())
-                //     .with(fmt::Layer::default().with_writer(non_blocking).event_format(format))
-                //     // .with(console_layer)
-                //     ;
-                // tracing::subscriber::set_global_default(logger).unwrap();
-
                 // apis全局初始化
                 service_utils::ApiUtils::init_all_api().await;
                 // 定时任务初始化
